e8dda4b089f2850bcd034d15a8a5962df49202c1
Tom Louis Mery <mery@izar.ib0.cm.izar.local>
2023-05-02 10:58:33 +0200

    add binary label encoder

--- src/models/label_encoder.py
+++ src/models/label_encoder.py
@@ -30,3 +30,34 @@ class LabelEncoder():
     
     def inv_transform(self, labels):
         return [self.decode_map[label] for label in labels]
+
+
+class BinaryLabelEncoder():
+    def __init__(self):
+        self.encode_map = {
+            'Eating': 0,
+            'Face touching': 0,
+            'Eye rubbing': 1,
+            'Eye rubbing light': 1,
+            'Eye rubbing moderate': 1,
+            'Eye touching': 0,
+            'Glasses readjusting': 0,
+            'Hair combing': 0,
+            'Make up': 0,
+            'Make up application': 0,
+            'Make up removal': 0,
+            'Skin scratching': 0,
+            'Teeth brushing': 0,
+            'Nothing': 0,
+            'no_label': 0
+        }
+        self.decode_map = {
+            0: "Nothing",
+            1: "Eye rubbing"
+        }
+    
+    def transform(self, labels):
+        return [self.encode_map[label] for label in labels]
+    
+    def inv_transform(self, labels):
+        return [self.decode_map[label] for label in labels]
